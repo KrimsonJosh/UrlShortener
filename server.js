4020d12f7006cac2137ef2497841b9944401e11f
KrimsonJosh <jumoinzad@gmail.com>
2024-12-21 04:27:48 -0600

    feat: added mongo schema/logic

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const app = express() 
+const mongoose = require('mongoose')
+const ShortUrl = require('./models/shortUrl')
+
+mongoose.connect('mongodb://localhost/urlShortener' , {
+    useNewUrlParser: true, useUnifiedTopology: True
+})
+
 app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/' , (req,res) => {
     res.render('index')
+})
+
+app.post('/shortUrls', (req,res) => {
+    ShortUrl.create({ full: req.body.fullUrl})
 })
 
 app.listen(process.env.PORT || 5000) 
